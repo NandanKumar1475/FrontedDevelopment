e3b1c32da2b17bb1cfeb6647cb06dd4813e6c3a8
Nandan <nandankumar1475@gmail.com>
2024-11-05 22:03:22 +0530

    Hbo fronted website

---
+++ JavaScript/JsPractise/dayone/intro2.js
@@ -0,0 +1,7 @@
+console.log("hii i ma virat");
+console.log("hii i ma Anushka");
+console.log("hii i ma  kohali");
+
+console.log("hii i ma Rohit");
+console.log("hii i ma garden owner   ");
+console.log("hii i ma  sharma ");
