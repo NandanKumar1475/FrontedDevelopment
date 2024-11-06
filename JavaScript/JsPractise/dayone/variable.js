939b25dc71628940bae58355c41822fa3a1d646e
Nandan <nandankumar1475@gmail.com>
2024-11-06 11:21:02 +0530

    into variables in js

---
+++ JavaScript/JsPractise/dayone/variable.js
@@ -0,0 +1,41 @@
+// variable keywords are var let const
+
+// 1.var 
+console.log("=========================VAR====================================")
+var a; //decalreation
+a = 10; // initalisation
+
+console.log(a);
+a = 20; // re-declare 
+console.log(a);
+
+var a = 30 ; // re-declare 
+console.log(a);
+
+console.log("========================LET====================================")
+// 2. let 
+console.log("Let keyword");
+ let b ; 
+ b = 30;
+ console.log(b);
+ b= 40 ; // re-initalise
+ console.log(b);
+// let b =40 ; can not re-declare'
+console.log("=========================CONST====================================")
+// 3. const 
+console.log('const');
+const c = 100;
+console.log(c);
+// c =200; // can not re-declare
+// const c= 200 // can not re-initalise
+
+console.log("==============================================================")
+
+
+ 
+
+
+
+
+
+
