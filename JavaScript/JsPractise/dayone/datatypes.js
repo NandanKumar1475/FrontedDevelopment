497d737377fd1275de41d7d552875fc62f7fb8f9
Nandan <nandankumar1475@gmail.com>
2024-11-09 11:43:25 +0530

    spoti-clone-landing-page

---
+++ JavaScript/JsPractise/dayone/datatypes.js
@@ -0,0 +1,37 @@
+let a = 10;
+console.log(typeof a);
+
+let b = 10.0;
+console.log(typeof b);
+
+let c = -23;
+console.log(typeof c);
+
+let d = +23;
+console.log(typeof d);
+
+let str = "";
+console.log(typeof str);
+let str1 = "1023";
+console.log(typeof str1);
+let str3 = "heloo";
+console.log(typeof str3);
+let e = null;
+console.log(typeof e);
+let f ;
+console.log(typeof f);
+let str2 = "40"
+console.log(10+str1);
+console.log(str3+10)
+console.log(10+str3);
+console.log(10-str3);
+console.log(str2-10);
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/JsPractise/dayone/operator.js
@@ -0,0 +1,24 @@
+// console.log("===================Assinement Operatot=============");
+// let a = 10;
+// let b = 10;
+// console.log(a==b);
+// console.log(a!=b);
+
+// console.log("=======================String comaprison=============")
+// let c = 20;
+// let d ="20";
+// console.log(c===d);
+// console.log(c!==b);
+
+console.log("Task1");
+let m =10 ;
+let n =20;
+var a = m/n;
+var a= a-m;
+m = a+ 30;
+let b= a*n-m;
+a= b*a/n;
+b = a+n*m;
+console.log(a,b,m,n);
+
+
